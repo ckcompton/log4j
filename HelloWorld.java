5ec378b75fe3ec2cd7d935affad0bc3758b20448
ckcompton <ckcompton@yahoo.com>
2022-07-09 01:32:20 +0000

    this is not working

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 import org.apache.log4j.Logger;
-
+//import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Appender;
+import org.apache.log4j.PatternLayout;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
@@ -8,11 +12,24 @@ public class log4jExample{
 
    /* Get actual class name to be printed on */
    static Logger log = Logger.getLogger(log4jExample.class.getName());
-
    public static void main(String[] args)throws IOException,SQLException{
-      log.debug("Hello this is a debug message");
+             // creates pattern layout
+        PatternLayout layout = new PatternLayout();
+        String conversionPattern = "%-7p %d [%t] %c %x - %m%n";
+        layout.setConversionPattern(conversionPattern);
+ 
+ 
+        // creates file appender
+        FileAppender fileAppender = new FileAppender();
+        fileAppender.setFile("applog3.txt");
+        fileAppender.setLayout(layout);
+        fileAppender.activateOptions();
+	  
+	   log.debug("Hello this is a debug message");
       log.info("Hello this is an info message");
-	System.out.println("HELLO");
+      log.error("Hellow");
+
+      System.out.println("HELLO");
 
    }
 }
--- command
+++ command
@@ -1,1 +1,4 @@
+java  -cp "/home/ec2-user/project/lib/*"  /home/ec2-user/project/HelloWorld.java -Dlog4j.configurationFile="/home/ec2-user/project/log4j2.xml"
+
+
 java -cp "/home/ec2-user/project/lib/*"  /home/ec2-user/project/HelloWorld.java
